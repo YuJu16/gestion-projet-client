cecb1c8c7137ed243138e52fd4bfda00604f0f5a
gjulia <julia.gari75@ynov.com>
2026-07-14 11:52:28 +0200

    chore: setup vite react typescript et tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Test Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
